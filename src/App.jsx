c3d3b7d43718655a1b86ed178bb100eba9955bb4
kyle <kyle.lewis@ust.com>
2024-11-29 21:09:21 -0800

    init

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div>
+        <div>
+          <img src="https://www.sddistillery.com/logo-500w.png" />
+        </div>
+        <p>PRESENTS</p>
+        <h2
+          style={{ fontWeight: "lighter", fontSize: "48px", padding: "30px 0" }}
+        >
+          Our 10th Anniversary Party!
+        </h2>
+        <h1>HOLIDAY JAM</h1>
+        <div>
+          <img src="./src/assets/bourbon-ad-2.png" />
+        </div>
+        <h2 style={{ fontWeight: "lighter" }}>Saturday, December 21st 2024</h2>
+        <h3>@ 7:00PM</h3>
+
+        <div></div>
+      </div>
+    </>
+  );
+}
+
+export default App;
